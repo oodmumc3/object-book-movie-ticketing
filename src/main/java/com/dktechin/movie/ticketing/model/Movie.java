008275c650e799ffbef353cd2bf30f515c3c39f1
jin.bak <jin.bak@kakaocorp.com>
2020-01-06 10:05:33 +0900

    특수한 상황(할인 정책이 없는경우)을 처리하는 부분 삭제

--- src/main/java/com/dktechin/movie/ticketing/model/Movie.java
+++ src/main/java/com/dktechin/movie/ticketing/model/Movie.java
@@ -16,7 +16,7 @@ public class Movie {
     /** * 영화 관람 금액 */
     @Getter private final Money fee;
     /** * 영화 할인 정책 정보 */
-    private final DiscountPolicy discountPolicy;
+    private DiscountPolicy discountPolicy;
 
     /**
      * 상영 정보에 따른 할인 정책을 고려해 영화 관람 가격을 계산후 반환한다.
@@ -24,11 +24,10 @@ public class Movie {
      * @return
      */
     public Money calculateMovieFee(Screening screening) {
-        // 할인 정책이 없는 특수한 상황시 기존 금액을 반환한다.
-        if (this.discountPolicy == null) {
-            return this.fee;
-        }
-
         return this.fee.minus(this.discountPolicy.calculateDiscountAmount(screening));
+    }
+
+    public void changeDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
     }
 }
